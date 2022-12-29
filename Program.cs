e6e0ebbe2c6851d09a644e3390a591bc8bbbda7b
BreyneWard <wardbreyne@hotmail.com>
2022-12-29 17:54:22 +0100

    10.6. bankmanager_deel2

--- Program.cs
+++ Program.cs
@@ -6,10 +6,24 @@ class Program
     {
         Rekening r1 = new Rekening();
         Rekening r2 = new Rekening();
+        r1.StortGeld(1000);
+        r2.StortGeld(1000);
+        
+        Rekening.SimuleerOverdracht(r1, r2);
+        Console.WriteLine($"Saldo rekening 1 na simuleeroverdracht: {r1.Balans}");    
+        Console.WriteLine($"Saldo rekening 2 na simuleeroverdracht: {r2.Balans}");
+
+
+
+
         Rekening kinderRekening = new Rekening("test");
-        Console.WriteLine($"Kinderrekening naam: {kinderRekening.NaamKlant} en balans is {kinderRekening.Balans}");
+        Console.WriteLine($"Kinderrekening met overloaded constructor: {kinderRekening.NaamKlant} en balans is {kinderRekening.Balans}");
         Rekening test1 = Rekening.CreeerTienerRekening("test1");
-        Console.WriteLine($"Kinderrekening naam1: {test1.NaamKlant} en balans is {test1.Balans}");
+        Console.WriteLine($"Kinderrekening test1 static method in Rekening class: {test1.NaamKlant} en balans is {test1.Balans}");
+        Rekening test2 = CreeerTienerRekening1("test2");
+
+        Console.WriteLine($"Kinderrekening test2 static method outside Rekening class: {test2.NaamKlant} en balans is {test2.Balans}");
+        
         string klantenNaam1 = "Pol";
         string klantenNaam2 = "Piet";
         string rekeningNummer1 = "03085464";
@@ -38,6 +52,11 @@ class Program
 
     }
 
+    public static Rekening CreeerTienerRekening1(string naam)
+    {   
+        return new Rekening(naam);
+    }
+
     // Classes
     public class Rekening
     {
@@ -64,8 +83,26 @@ class Program
         // Methods
         
 
-        public void SimuleerOverdracht(Rekening a, Rekening B)
+        public static void SimuleerOverdracht(Rekening a, Rekening b)
         {
+            Random amount = new Random();
+            for(int i=0; i<5; i++)
+            {
+                int bedrag = amount.Next(0,101);
+            if(i % 2 == 0)
+                {
+                   
+                   a.StortGeld(b.HaalGeldAf(bedrag));
+                    Console.WriteLine($"Rekening R2 stort {bedrag} op rekening R1");
+                }
+                else
+                {
+                    b.StortGeld(a.HaalGeldAf(bedrag)); 
+                    Console.WriteLine($"Rekening R1 stort {bedrag} op rekening R2");
+                }
+
+            }
+            
 
         }
 
